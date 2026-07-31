658c21f4b306a7e64d31838a2ea738704ad47e58
AhmadDabour <ahmadmdabour@gmail.com>
2026-07-30 21:32:40 -0400

    test

--- src/main.cpp
+++ src/main.cpp
@@ -59,7 +59,6 @@ int main() {
        buffer[bytesRead] = '\0';
        std::cout << "Received " << bytesRead << " bytes: " << buffer << std::endl;
 
-       // Create an empty response
        std::vector<unsigned char> response = { 4, 210, 128, 0, 0, 1, 0, 1, 0, 0, 0, 0, 12, 'c', 'o', 'd', 'e', 'c', 'r', 'a', 'f', 't', 'e', 'r', 's', 2, 'i', 'o', '\x00', 0, 1, 0, 1, 12, 'c', 'o', 'd', 'e', 'c', 'r', 'a', 'f', 't', 'e', 'r', 's', 2, 'i', 'o', '\x00', 0, 1, 0, 1, 0, 0, 0, 60, 0, 4, 8, 8, 8, 8, '\x00' };
 
        // Send response
