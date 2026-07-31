ffae4a5e5f0edd3e15b86307e8c89a0eb0faa023
AhmadDabour <ahmadmdabour@gmail.com>
2026-07-30 21:34:00 -0400

    test2

--- src/main.cpp
+++ src/main.cpp
@@ -61,7 +61,6 @@ int main() {
 
        std::vector<unsigned char> response = { 4, 210, 128, 0, 0, 1, 0, 1, 0, 0, 0, 0, 12, 'c', 'o', 'd', 'e', 'c', 'r', 'a', 'f', 't', 'e', 'r', 's', 2, 'i', 'o', '\x00', 0, 1, 0, 1, 12, 'c', 'o', 'd', 'e', 'c', 'r', 'a', 'f', 't', 'e', 'r', 's', 2, 'i', 'o', '\x00', 0, 1, 0, 1, 0, 0, 0, 60, 0, 4, 8, 8, 8, 8, '\x00' };
 
-       // Send response
        if (sendto(udpSocket, response.data(), response.size(), 0, reinterpret_cast<struct sockaddr*>(&clientAddress), sizeof(clientAddress)) == -1) {
            perror("Failed to send response");
        }
