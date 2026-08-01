30344db10c9156021252c4e9904824b9b79dcfa1
AhmadDabour <ahmadmdabour@gmail.com>
2026-08-01 17:58:41 -0400

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <vector>
+#include <arpa/inet.h>
 
 std::vector<unsigned char> header_parser(char buffer[512]) {
     std::vector<unsigned char> temp;
@@ -34,32 +35,72 @@ std::pair<std::vector<unsigned char>, int> read_label(char buffer[512], int fina
     return {temp, finalpos};
 }
 
-std::vector<unsigned char> handle_answer(std::vector<unsigned char> temp) {
-    std::vector <unsigned char> answers = temp;
-    answers.insert(answers.end(), { 0, 0, 0, 60, 0, 4, 8, 8, 8, 8 });
-    return answers;
-}
-std::vector<unsigned char> question_answer_parser(char buffer[512], int bytesRead) { 
+// std::vector<unsigned char> handle_answer(std::vector<unsigned char> temp) {
+//     std::vector <unsigned char> answers = temp;
+//     answers.insert(answers.end(), { 0, 0, 0, 60, 0, 4, 8, 8, 8, 8 });
+//     return answers;
+// }
+std::vector<unsigned char> question_answer_parser(char buffer[512], int bytesRead, int resolver_socket, sockaddr_in resolver_addr, char resolver_buffer[512], int resolver_bytes, socklen_t clientAddrLen) {
     std::vector<unsigned char> temp_answers;
     std::vector<std::vector<unsigned char>> questions;
     int id;
     int qdcount = buffer[5];
     int pos = 12;
-    
+
     for (int q = 0; q < qdcount; q++) {
+        std::vector<unsigned char> temp_query = { 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0 };
+        temp_query[0] = buffer[0];
+        temp_query[1] = buffer[1];
         auto [name, endpos] = read_label(buffer, pos);
         for (int i = endpos + 1; i < endpos + 5; i++) { name.push_back(buffer[i]); }
+        temp_query.insert(temp_query.end(), name.begin(), name.end());
+        sendto(resolver_socket, temp_query.data(), temp_query.size(), 0, reinterpret_cast<struct sockaddr*>(&resolver_addr), sizeof(resolver_addr));
+        resolver_bytes = recvfrom(resolver_socket, resolver_buffer, 512, 0, reinterpret_cast<struct sockaddr*>(&resolver_addr), &clientAddrLen);
+        if (resolver_bytes == -1) {
+            perror("Erorr recieving data from resolver");
+            continue;
+        }
+        auto [throwaway, idx] = read_label(resolver_buffer, 12);
+        idx += 5;
+        for (int i = idx; i < resolver_bytes; i++) {
+            temp_answers.push_back(resolver_buffer[i]);
+        }
         questions.push_back(name);
         pos = endpos + 5;
     }
 
     std::vector<unsigned char> res;
     for (auto &q : questions) { res.insert(res.end(), q.begin(), q.end()); }
-    for (auto &q : questions) { auto a = handle_answer(q); res.insert(res.end(), a.begin(), a.end()); }
+    res.insert(res.end(), temp_answers.begin(), temp_answers.end());
+    // for (auto &q : questions) { auto a = handle_answer(q); res.insert(res.end(), a.begin(), a.end()); }
     return res;
 }
 
-int main() {
+int main(int argc, char* argv[]) {
+    std::string ip;
+    int port_num;
+    // Extracting resolver ip and port
+    for (int i = 0; i < argc; i++) {
+        if (std::string(argv[i]) == "--resolver") {
+            std::string addr = std::string(argv[i+1]);
+            int pos = addr.find(":");
+            
+            ip = addr.substr(0, pos); // extracts all characters from index 0 to the colon in ip:port
+            std::string port = addr.substr(pos + 1); // extracts all characters starting past the colon and running to the end
+
+            port_num = std::stoi(port);
+            
+            
+        }
+    }
+
+    sockaddr_in resolver_addr = {}; 
+    resolver_addr.sin_family = AF_INET;
+    resolver_addr.sin_port = htons(port_num);
+    inet_pton(AF_INET, ip.c_str(), &resolver_addr.sin_addr);    
+
+    int resolver_socket = socket(AF_INET, SOCK_DGRAM, 0);
+    
     // Flush after every std::cout / std::cerr
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
@@ -101,6 +142,8 @@ int main() {
    int bytesRead;
    char buffer[512];
    socklen_t clientAddrLen = sizeof(clientAddress);
+   char resolver_buffer[512];
+   int resolver_bytes;
 
    while (true) {
        // s
@@ -112,9 +155,9 @@ int main() {
 
        buffer[bytesRead] = '\0';
        std::cout << "Received " << bytesRead << " bytes: " << buffer << std::endl;
-       
+
        std::vector<unsigned char> response = header_parser(buffer);
-       std::vector<unsigned char> question_and_answer = question_answer_parser(buffer, bytesRead);
+       std::vector<unsigned char> question_and_answer = question_answer_parser(buffer, bytesRead, resolver_socket, resolver_addr, resolver_buffer, resolver_bytes, clientAddrLen);
        for (int i = 0; i < question_and_answer.size(); i++) {
            response.push_back(question_and_answer[i]);
        }
